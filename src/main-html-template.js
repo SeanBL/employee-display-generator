ed6f25b4b6ba25d4ee5121557b619a1fd424bca2
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-11-19 21:38:50 -0800

    added manager constructor to promise

---
+++ src/main-html-template.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/manager");
+
+const generateHtml = ({}) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Employees</title>
+<head>
+<body>
+    <header>
+        <h1 class="header">My Team</h1>
+    </header>
+    <div class="flex-container">
+        <div></div>
+        <div>
+    
+    </div>
+`;
+
+module.exports = generateHtml;
---
+++ src/manager-html-template.js
@@ -0,0 +1,8 @@
+
+function managerInfo(manager) {
+let managerBox = `<div>${manager.name}</div>`
+
+ console.log(manager.name);
+
+}
+module.exports = managerInfo;
