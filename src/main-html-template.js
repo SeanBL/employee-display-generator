e91c4cdb6796bf4f0ac17db2fff6975ed8ccd27c
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-11-21 17:23:46 -0800

    added initial css function code

--- src/main-html-template.js
+++ src/main-html-template.js
@@ -7,7 +7,9 @@ generateHtml = (managerBox, engineerBox, internBox) => {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Employees</title>
 <head>
 <body>
@@ -23,7 +25,8 @@ generateHtml = (managerBox, engineerBox, internBox) => {
     <div>
         ${internBox}
     </div>
-    
+</body>
+</html>    
 `
 return htmlTemp;
 };
--- src/manager-html-template.js
+++ src/manager-html-template.js
@@ -1,12 +1,14 @@
 
 managerInfo = (manager) => {
 let managerBox = 
-`<div>${manager.name}</div>
+`<div class="flex-item">
+<div class="name">${manager.name}</div>
 <div>Manager</div>
 <div>
     <p>ID: ${manager.id}</P>
     <p>Email: <a href="mailto:${manager.email}"> ${manager.email} </a></span></P>
     <p>Office number: ${manager.officeNumber}</p>
+</div>
 </div>
 `
 
