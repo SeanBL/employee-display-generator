c4c3b11c01a70a0957c4c43c4010c2f402b8e426
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-11-23 22:18:30 -0800

    added flex wrap and updated html links

--- src/css-generator.js
+++ src/css-generator.js
@@ -7,17 +7,76 @@ generateCss = () => {
         background-color: rgb(255, 0, 0);
         text-align: center;
         padding: 40px;
+        margin-bottom: 10px;
     }
 
     .flex-container {
         display: flex;
         flex-direction: row;
-        font-size: 30px;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-left: 200px;
+        margin-right: 200px;
     }
 
     .flex-item {
-        border: 1px solid black;
+        box-shadow: 3px 3px 5px 1px gray;
+        margin-left: 15px;
+        margin-right: 15px;
+        margin-top: 25px;
+    }
+
+    .titles {
+        color: white;
+        background-color: rgb(4, 134, 255);
+        padding-bottom: 10px;
+        padding-left: 10px;
+        padding-top: 2px;
+        padding-right: 15px;
+    }
+
+    .name {
+        font-size: 23px;
+        margin-bottom: 5px;
+    }
+
+    .role {
+        font-size: 17px;
+        margin: 0px;
+    }
+
+    .icon{
+        margin-right: 5px;
+    }
+
+    .e-info {
         padding: 10px;
+        background-color: rgb(238, 238, 238);
+    }
+
+    .id {
+        border: 1px solid rgb(172, 172, 172);
+        padding: 7px;
+        font-size: 12px;
+        margin-bottom: 0px;
+    }
+
+    .email {
+        border: 1px solid rgb(172, 172, 172);
+        padding: 7px;
+        font-size: 12px;
+        margin: 0px;
+    }
+
+    a:link {
+        text-decoration: none;
+    }
+
+    .last-info {
+        border: 1px solid rgb(172, 172, 172);
+        padding: 7px;
+        font-size: 12px;
+        margin-top: 0px;
     }
     `
     return cssPage;
--- src/main-html-template.js
+++ src/main-html-template.js
@@ -9,6 +9,7 @@ generateHtml = (managerBox, engineerBox, internBox) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
     <title>Employees</title>
 <head>
@@ -18,11 +19,7 @@ generateHtml = (managerBox, engineerBox, internBox) => {
     </header>
     <div class="flex-container">
         ${managerBox}
-    </div>
-    <div>
         ${engineerBox}
-    </div>
-    <div>
         ${internBox}
     </div>
 </body>
